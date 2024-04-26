edc7bea2292a342094a7064d12b07730259d999e
Ubinquitous <ubinquitous1@gmail.com>
2024-04-26 11:18:50 +0900

    feat : setting global env

---
+++ src/lib/globalEnv.ts
@@ -0,0 +1,10 @@
+const environment = {
+  CLIENT_ID: process.env.NEXT_PUBLIC_CODEF_CLIENT_ID || "",
+  CLIENT_SECRET: process.env.NEXT_PUBLIC_CODEF_CLIENT_SECRET || "",
+  CLIENT_OAUTH_URL: process.env.NEXT_PUBLIC_CODEF_REQUEST_OAUTH_TOKEN_URI || "",
+  REDIRECT_URI: process.env.NEXT_PUBLIC_REDIRECT_URI || "",
+  PUBLIC_KEY: process.env.NEXT_PUBLIC_PUBLIC_KEY || "",
+  CODEF_ADMIN_URL: process.env.NEXT_PUBLIC_CODEF_ADMIN_URL || "",
+};
+
+export default environment;
