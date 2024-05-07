05db91374081c6761d9de473f7a362eac37c7e49
Ubinquitous <ubinquitous1@gmail.com>
2024-05-07 09:14:44 +0900

    chore : column publish

---
+++ src/components/Flex/Column.tsx
@@ -0,0 +1,24 @@
+import { styled } from "@linaria/react";
+import FlexPropsType from "./type";
+
+const Column = ({
+  children,
+  gap,
+  justifyContent = "none",
+  alignItems = "none",
+  width,
+  height,
+}: FlexPropsType) => {
+  return (
+    <StyledColumn style={{ gap, justifyContent, alignItems, width, height }}>
+      {children}
+    </StyledColumn>
+  );
+};
+
+export default Column;
+
+const StyledColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
