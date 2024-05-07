107e30a79e8fddffa5c787f9f985c993b5b767b5
Ubinquitous <ubinquitous1@gmail.com>
2024-05-07 09:15:47 +0900

    chore : initial styles

---
+++ src/styles/flex.ts
@@ -0,0 +1,56 @@
+import { CSSProperties } from "@linaria/core";
+
+interface PropsType {
+  direction?: CSSProperties["flexDirection"];
+  justify?: CSSProperties["justifyContent"];
+  align?: CSSProperties["alignItems"];
+}
+
+const flexGenerator = ({
+  direction = "unset",
+  align = "unset",
+  justify = "unset",
+}: PropsType = {}) => ({
+  display: "flex",
+  flexDirection: direction,
+  justifyContent: justify,
+  alignItems: align,
+});
+
+const flex = {
+  FLEX: flexGenerator(),
+  CENTER: flexGenerator({ align: "center", justify: "center" }),
+  VERTICAL: flexGenerator({ align: "center" }),
+  HORIZONTAL: flexGenerator({ justify: "center" }),
+  START: flexGenerator({ align: "center", justify: "flex-start" }),
+  BETWEEN: flexGenerator({ align: "center", justify: "space-between" }),
+  END: flexGenerator({ align: "center", justify: "flex-end" }),
+
+  COLUMN_FLEX: flexGenerator({
+    direction: "column",
+  }),
+  COLUMN_CENTER: flexGenerator({
+    direction: "column",
+    align: "center",
+    justify: "center",
+  }),
+  COLUMN_VERTICAL: flexGenerator({ direction: "column", align: "center" }),
+  COLUMN_HORIZONTAL: flexGenerator({ direction: "column", justify: "center" }),
+  COLUMN_START: flexGenerator({
+    direction: "column",
+    align: "center",
+    justify: "flex-start",
+  }),
+  COLUMN_BETWEEN: flexGenerator({
+    direction: "column",
+    align: "center",
+    justify: "space-between",
+  }),
+  COLUMN_END: flexGenerator({
+    direction: "column",
+    align: "center",
+    justify: "flex-end",
+  }),
+};
+
+export default flex;
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+const theme = {
+  primary: "#3B6DFF",
+  white: "#FFFFFF",
+  black: "#000000",
+  gray: "#AAAAAA",
+  lightgray: "#EEEEEE",
+};
+
+export default theme;
