5475b15279786e020e5bcca9f7d54e28c0108ad4
Ubinquitous <ubinquitous1@gmail.com>
2024-05-07 09:14:05 +0900

    chore : home page publish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+"use client";
 
-const Home = () => {
-  return <div>ascasds</div>;
+import React from "react";
+import * as L from "./style";
+import { Logo } from "~/components/icons";
+import { useRouter } from "next/navigation";
+
+const Splash = () => {
+  const router = useRouter();
+  setTimeout(() => router.push("/main"), 1500);
+
+  return (
+    <L.SplashContainer>
+      <Logo />
+    </L.SplashContainer>
+  );
 };
 
-export default Home;
+export default Splash;
---
+++ src/app/style.ts
@@ -0,0 +1,25 @@
+import { css } from "@linaria/core";
+import { styled } from "@linaria/react";
+import { flex, theme } from "~/styles";
+
+export const SplashContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: ${theme.primary};
+  ${flex.CENTER};
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: ${theme.white};
+  position: fixed;
+  top: 0;
+  right: 800px; /* 초기 위치 */
+  z-index: 2;
+  transition: right 3s ease;
+
+  :active {
+    right: 0;
+  }
+`;
