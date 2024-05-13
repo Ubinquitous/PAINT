f155192551e033022f9230376a499379082aeb66
Ubinquitous <ubinquitous1@gmail.com>
2024-05-13 15:54:10 +0900

    chore : dto 변경

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,9 +1,9 @@
 import { atom } from "jotai";
 
 export const signupContext = atom({
-  name: "",
-  birthday: "",
-  phoneNumber: "",
+  userName: "",
+  birthDate: "",
   certFile: "",
+  password: "",
   bankCodeList: [] as Array<string>,
 });
