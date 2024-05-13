0370101811635323be81414d390508f53e84154b
Ubinquitous <ubinquitous1@gmail.com>
2024-05-13 12:04:58 +0900

    feat : providers 정의

---
+++ src/app/providers.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { PropsWithChildren, useState } from "react";
+import { Provider as JotaiProvider } from "jotai";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const Providers = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+            retry: 0,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <JotaiProvider>{children}</JotaiProvider>
+    </QueryClientProvider>
+  );
+};
+
+export default Providers;
