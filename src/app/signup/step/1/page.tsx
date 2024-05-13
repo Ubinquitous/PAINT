e61b93fb5053b14e49c0c2ca5ed6514b662e8686
Ubinquitous <ubinquitous1@gmail.com>
2024-05-13 12:05:49 +0900

    chore : step 1 페이지 완성

---
+++ src/app/signup/step/1/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { ChangeEvent } from "react";
+import StepContainer from "../StepContainer";
+import Input from "~/components/atoms/Input";
+import Button from "~/components/atoms/Button";
+import { useAtom } from "jotai";
+import { signupContext } from "~/context";
+import { useRouter } from "next/navigation";
+import * as L from "./style";
+
+const NAME_MAX_LENGTH = 6;
+
+const SignupStep1 = () => {
+  const router = useRouter();
+  const [signup, setSignup] = useAtom(signupContext);
+
+  const handleProfileNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    if (value.length <= NAME_MAX_LENGTH)
+      setSignup((prev) => ({ ...prev, name: value }));
+  };
+
+  return (
+    <StepContainer
+      title={`가입을 축하드려요!\n어떻게 불러드리면 될까요?`}
+      description="꼭 실명을 입력해주세요!"
+    >
+      <L.Container>
+        <Input
+          value={signup.name}
+          onChange={handleProfileNameChange}
+          placeholder="이름을 입력해주세요"
+          maxLetterLength={6}
+          activeLetterCount
+        />
+        <Button
+          disabled={!signup.name}
+          onClick={() => router.push("/signup/step/2")}
+        >
+          다음
+        </Button>
+      </L.Container>
+    </StepContainer>
+  );
+};
+
+export default SignupStep1;
---
+++ src/app/signup/step/1/style.ts
@@ -0,0 +1,9 @@
+import { styled } from "@linaria/react";
+import { flex } from "~/styles";
+
+export const Container = styled.figure`
+  width: 100%;
+  height: 100%;
+  padding-top: 50%;
+  ${flex.COLUMN_BETWEEN};
+`;
