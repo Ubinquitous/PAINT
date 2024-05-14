dba0473424ecaa749f9fc8e1577bf2476dc16565
Ubinquitous <ubinquitous1@gmail.com>
2024-05-14 09:01:08 +0900

    chore : interceptor로 백엔드 토큰 재발급 구현

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,6 +4,20 @@ export const paint = axios.create({
   baseURL: "/api/",
 });
 
+paint.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const { config, response } = error;
+    if (response.status === 500 && !config.sent) {
+      config.sent = true;
+      await paint.post("/token/connected");
+      paint(config);
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export const authorization = () => ({
   headers: {
     Authorization: localStorage.getItem("access_token"),
