edccf97d62757218fd07518877d02d78d2a06bf6
ubinquitous <ubinquitous1@gmail.com>
2024-06-18 11:55:00 +0900

    chore : prisma db로 이전

--- src/app/api/account/list/GetAccountListService.ts
+++ src/app/api/account/list/GetAccountListService.ts
@@ -1,51 +1,27 @@
-import { NextRequest, NextResponse } from "next/server";
-import { codefAuthorization } from "~/lib/codefAuthorization";
-import { decodeToJson } from "~/lib/decodeToJson";
-import { ConnectedCommonVerifyRequestDto } from "~/lib/dto/ConnectedCommonVerifyRequestDto";
+import { NextResponse } from "next/server";
 import { getAuthorizationToken } from "~/lib/getAuthorizationToken";
 import { jwtUtils } from "~/lib/jwtUtils";
 import { prismaClient } from "~/lib/prismaClient";
-import { codef } from "../..";
 
 class GetAccountListService {
-  public async execute(req: NextRequest) {
+  public async execute() {
     const { connectedId } = jwtUtils().verify(getAuthorizationToken());
 
-    const data = await prismaClient.user.findUnique({ where: { connectedId } });
-    const organizationList = JSON.parse(data?.organization || "[]");
-
-    const accountList = {
-      resDepositTrust: [] as any,
-      resForeignCurrency: [] as any,
-      resFund: [] as any,
-      resLoan: [] as any,
-      resInsurance: [] as any,
-    };
-
-    for (const organization of organizationList) {
-      const { data } = decodeToJson(
-        await this.getAccountList({ connectedId, organization })
-      );
-
-      if (data.resDepositTrust.length)
-        accountList.resDepositTrust.push(data.resDepositTrust);
-      if (data.resForeignCurrency.length)
-        accountList.resForeignCurrency.push(data.resForeignCurrency);
-      if (data.resFund.length) accountList.resFund.push(data.resFund);
-      if (data.resLoan.length) accountList.resLoan.push(data.resLoan);
-      if (data.resInsurance.length)
-        accountList.resInsurance.push(data.resInsurance);
-    }
-
-    return NextResponse.json({ status: 200, accountList });
-  }
-
-  private async getAccountList(request: ConnectedCommonVerifyRequestDto) {
-    const { data } = await codef.get("/v1/kr/bank/p/account/account-list", {
-      data: request,
-      ...(await codefAuthorization()),
+    const data = await prismaClient.account.findMany({
+      where: { connectedId },
+      select: {
+        id: true,
+        organization: true,
+        accountName: true,
+        accountNumber: true,
+        accountDisplay: true,
+        accountBalance: true,
+        accountCreatedAt: true,
+        accountRefreshedAt: true,
+      },
     });
-    return data;
+
+    return NextResponse.json({ status: 200, data });
   }
 }
 
