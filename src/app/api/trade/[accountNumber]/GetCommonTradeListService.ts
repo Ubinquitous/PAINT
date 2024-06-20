e36d48fb665bdf871047f8d4e43d355fbc51e6c4
ubinquitous <ubinquitous1@gmail.com>
2024-06-20 10:05:23 +0900

    update : common trade account information added

--- src/app/api/trade/[accountNumber]/GetCommonTradeListService.ts
+++ src/app/api/trade/[accountNumber]/GetCommonTradeListService.ts
@@ -13,11 +13,25 @@ class GetCommonTradeListService {
         where: { accountNumber, connectedId },
       })) || {};
 
-    const data = await prismaClient.trade.findMany({
+    const account = await prismaClient.account.findUnique({
+      where: { accountNumber },
+      select: {
+        id: true,
+        organization: true,
+        accountName: true,
+        accountNumber: true,
+        accountDisplay: true,
+        accountBalance: true,
+        accountCreatedAt: true,
+        accountRefreshedAt: true,
+      },
+    });
+
+    const tradeList = await prismaClient.trade.findMany({
       where: { accountId },
     });
 
-    return NextResponse.json({ status: 200, data });
+    return NextResponse.json({ status: 200, account, tradeList });
   }
 }
 
