23602820003a1499a0be60f603291b4733043e98
ubinquitous <ubinquitous1@gmail.com>
2024-06-22 21:04:56 +0900

    fix : delete test case

--- src/app/api/analysis/GetAverageScoreService.ts
+++ src/app/api/analysis/GetAverageScoreService.ts
@@ -12,48 +12,17 @@ class GetAverageScoreService {
     const list: Array<any> =
       await prismaClient.$queryRaw`SELECT SUM(amount) AS amount FROM Trade t, Account a WHERE a.id = t.accountId and t.category = '-' GROUP BY a.connectedId;`;
 
-    console.log(myExpenditure.amount.d[0]);
-    console.log(list);
-    // Step 1: Extract amounts and convert to numbers
-    const data = [
-      { amount: "1245789" },
-      { amount: "1834567" },
-      { amount: "1578934" },
-      { amount: "1987654" },
-      { amount: "1765432" },
-      { amount: "1123456" },
-      { amount: "1324567" },
-      { amount: "1456789" },
-      { amount: "1623456" },
-      { amount: "1543210" },
-      { amount: "1897654" },
-      { amount: "1267766" },
-      { amount: "1234567" },
-      { amount: "1767890" },
-      { amount: "1127890" },
-      { amount: "1000000" },
-      { amount: "1654321" },
-      { amount: "1981234" },
-      { amount: "1712345" },
-      { amount: "1865432" },
-      { amount: "1498765" },
-      { amount: "1789123" },
-    ];
-
-    const amounts = data.map((item) => Number(item.amount));
-
-    amounts.sort((a, b) => a - b);
-    console.log(amounts);
+    const amounts = list
+      .map((item) => item.amount.d[0])
+      .map((item) => Number(item.amount))
+      .sort((a, b) => a - b);
 
     const myAmount = Number(myExpenditure.amount.d[0]);
     const rank = amounts.findIndex((amount) => amount === myAmount) + 1;
-
-    console.log(rank);
     const percentage = ((amounts.length - rank) / amounts.length) * 100;
+    let score = Math.round(percentage);
 
-    const score = Math.round(percentage);
-    console.log(`Your expenditure is in the top ${percentage}% of the list.`);
-    console.log(`Your score is: ${100 - score}`);
+    if (score < 100) score += 1;
 
     return NextResponse.json({
       status: 200,
