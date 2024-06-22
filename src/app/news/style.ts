65f049f5c4a09ac0dce9a5d2da3602a508f3a30c
ubinquitous <ubinquitous1@gmail.com>
2024-06-22 21:48:13 +0900

    chore : news

---
+++ src/app/news/style.ts
@@ -0,0 +1,72 @@
+import { styled } from "@linaria/react";
+import Image from "next/image";
+import { flex, font, theme } from "~/styles";
+
+export const Container = styled.div`
+  width: 100%;
+  height: fit-content;
+  background-color: ${theme.primary};
+  gap: 16px;
+  overflow-y: scroll;
+  padding-bottom: 14%;
+  ${flex.COLUMN_FLEX};
+`;
+
+export const Header = styled.header`
+  color: ${theme.white};
+  ${flex.COLUMN_FLEX};
+  gap: 24px;
+  padding: 26px 40px;
+`;
+
+export const LogoWrap = styled.figure`
+  align-self: flex-end;
+`;
+
+export const Title = styled.h1`
+  color: ${theme.white};
+  ${font.D3};
+`;
+
+export const SubTitle = styled.p`
+  color: ${theme.white};
+  ${font.H2};
+  font-weight: 500;
+`;
+
+export const Body = styled.main`
+  width: 100%;
+  height: 100%;
+  background-color: ${theme.white};
+  border-top-right-radius: 20px;
+  border-top-left-radius: 20px;
+  padding: 30px;
+  gap: 32px;
+  ${flex.COLUMN_FLEX};
+`;
+
+export const BodyScroller = styled.div`
+  width: 100%;
+  height: 10%;
+`;
+
+export const BannerImage = styled(Image)`
+  width: 100%;
+  height: auto;
+`;
+
+export const AccountText = styled.h1`
+  ${font.H2};
+`;
+
+export const BankList = styled.ul`
+  gap: 46px;
+  width: 100%;
+  ${flex.COLUMN_FLEX};
+`;
+
+export const NewsList = styled.ul`
+  width: 100%;
+  gap: 22px;
+  ${flex.COLUMN_FLEX};
+`;
