07cfd9099fc1c6201997d602f6a0752c4dfaffdc
ubinquitous <ubinquitous1@gmail.com>
2024-06-29 21:01:19 +0900

    fix : to dvh

--- src/app/style.ts
+++ src/app/style.ts
@@ -4,14 +4,14 @@ import { flex, theme } from "~/styles";
 
 export const SplashContainer = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100dvh;
   background-color: ${theme.primary};
   ${flex.CENTER};
 `;
 
 export const Container = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100dvh;
   background-color: ${theme.white};
   position: fixed;
   top: 0;
